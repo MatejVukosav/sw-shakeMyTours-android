6a3f1a53058601f901b8d2b00829125faa0ea7e7
Florian Chauveau <f.chauveau@code-troopers.com>
2015-12-05 01:48:18 +0100

    dead code

--- app/src/main/java/com/codetroopers/shakemytours/core/modules/HomeActivityModule.java
+++ app/src/main/java/com/codetroopers/shakemytours/core/modules/HomeActivityModule.java
@@ -1,10 +1,6 @@
 package com.codetroopers.shakemytours.core.modules;
 
 import android.app.Activity;
-import android.content.Context;
-
-import com.codetroopers.shakemytours.core.components.ActivityScope;
-import com.codetroopers.shakemytours.example.DummyContentFactory;
 
 import dagger.Module;
 import dagger.Provides;
@@ -24,9 +20,4 @@ public class HomeActivityModule {
 
     // TODO put your application-specific providers here!
 
-    @ActivityScope
-    @Provides
-    protected DummyContentFactory provideDummyContentFactory(@ForApplication Context context) {
-        return new DummyContentFactory(context);
-    }
 }
